eadc4b435e2353e003175bc72aa63b335ce294f8
Jainam <91124384+jainam03@users.noreply.github.com>
2022-07-12 15:42:49 +0530

    added generator between 100 to 200

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let field1El = document.getElementById("field1");
 let field2El = document.getElementById("field2");
+let field3El = document.getElementById("field3");
 
 
 function generate1() {
@@ -12,4 +13,10 @@ function generate2() {
     let result2 = 0
     result2 += Math.floor(Math.random() * 51) + 50
     field2El.textContent = "Random number -> " + result2
+}
+
+function generate3() {
+    let result3 = 0
+    result3 += Math.floor(Math.random() * 101) + 100
+    field3El.textContent = "Random number -> " + result3
 }
