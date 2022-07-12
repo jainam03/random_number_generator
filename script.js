efee46b3e8f539fb81f210cfabc77f4b4d17b2cd
Jainam <91124384+jainam03@users.noreply.github.com>
2022-07-12 15:34:21 +0530

    .

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let field2El = document.getElementById("field2");
 function generate1() {
     let result1 = 0
     result1 += Math.floor(Math.random() * 50)
-    field1El.textContent = "Random number: " + result1
+    field1El.textContent = "Random number -> " + result1
 }
 
 function generate2() {
     let result2 = 0
     result2 += Math.floor(Math.random() * 51) + 50
-    field2El.textContent = "Random number: " + result2
+    field2El.textContent = "Random number -> " + result2
 }
